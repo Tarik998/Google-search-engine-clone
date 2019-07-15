6bbf706a704382f7269b363e6baa5e805047bb51
Tarik Mehmedovic <mehmedovict@outlook.com>
2019-07-15 21:18:33 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<div class="wrapper indexPage">
+		<div class="mainSection">
+
+			 <div class="logoContainer">
+			 		<img src="https://www.google.com.mx/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+			 </div> 
+
+
+		     <div class="searchContainer">
+				<form action="search.php" method="GET">	
+
+		     		<input class="searchBox" type="text" name="term">
+		      		<input class="searchButton" type="submit" value="Search">
+
+				</form>
+
+		     </div>
+
+
+		</div>
+
+
+
+
+
+</div>
+</body>
+</html>
